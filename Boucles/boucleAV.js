f918ac63336b44b78a468daffb5e2c05cef4a77b
MAROUAELMILOUDI <elm886825@gmail.com>
2026-09-21 16:45:32 +0200

    Initial main

--- Boucles/boucleAV.js
+++ Boucles/boucleAV.js
@@ -1,11 +1,15 @@
 const prompt = require('prompt-sync')()
 let n = Number(prompt("Entrer le nombre des lignes à composer: "))
 let etoile = "*"
-let etoile_espace = etoile.split(" ");
-for(i=0; i<n; i++){
-    console.log(etoile)
-    etoile+="**"
-}
-for(i = 0; i < etoile_espace.length; i++){
-    etoile_espace[i]+="&nbsp;"
+for(let i = 0 ; i < n; i++){
+  line = ""
+  for(let j = 0 ; j < n*2 ; j++){
+    if(j >= ((n*2/2)-i) && j <= ((n*2/2)+i)){
+      line+="*"
+    }
+    else{
+      line+=" "
+    }etoile+="**"
+  }
+  console.log(line)  
 }
