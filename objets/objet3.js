618a670ff3fedacee6741a4281b19623dda96c10
MAROUAELMILOUDI <elm886825@gmail.com>
2026-09-21 21:22:09 +0200

    Initial main

---
+++ objets/objet3.js
@@ -0,0 +1,7 @@
+let rectongle = {
+    largeur: "2",
+    longueur: "4",
+}
+console.log(`rectongle : ${rectongle.largeur} \n ${rectongle.longueur}`);
+let aire = rectongle.largeur * rectongle.longueur;
+console.log(`L'Aire de votre rectongle est: ${aire}`)
