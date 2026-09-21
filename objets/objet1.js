f45d11e1e57ebc030ec5bd414b4abe6dad40decb
MAROUAELMILOUDI <elm886825@gmail.com>
2026-09-21 19:30:03 +0200

    Initial main

---
+++ objets/objet1.js
@@ -0,0 +1,9 @@
+let personne = {
+    nom: "Elmiloudi",
+    prenom: "Maroua",
+    age: 19
+};
+console.log("Informations de la personne :");
+console.log(`Nom : ${personne.nom}`);
+console.log(`Prénom : ${personne.prenom}`);
+console.log(`Âge : ${personne.age} ans`);
