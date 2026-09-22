9e36c6562df6ba3f9defcd8f4f2b8acbb9e06c3d
MAROUAELMILOUDI <elm886825@gmail.com>
2026-09-22 14:21:32 +0200

    Initial main

---
+++ fonctions/fonction8.js
@@ -0,0 +1,10 @@
+function PairImpair(x){
+    if(x%2===0){
+        return true
+    } else{
+        return false
+    }
+}
+console.log(PairImpair(5))
+console.log(PairImpair(66))
+console.log(PairImpair(4))
