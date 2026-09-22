f55a5ef997dde2f42dcbdd1e9568208983a3d05e
MAROUAELMILOUDI <elm886825@gmail.com>
2026-09-22 12:32:59 +0200

    Initial main

---
+++ fonctions/fonction5.js
@@ -0,0 +1,18 @@
+function factorielle(x){
+    let res1 = 1;
+    for (let i = 1; i <= (x); i++) {
+        res1 *= i;
+    }
+    return res1;
+}
+
+console.log(factorielle(5))
+function factorielle(y){
+    let res2 = 1;
+    for (let i = 1; i <= (y); i++) {
+        res2 *= i;
+    }
+    return res2;
+}
+
+console.log(factorielle(4))
